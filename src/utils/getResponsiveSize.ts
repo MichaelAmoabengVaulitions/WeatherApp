452fbd5b40ff49534627cb9dad8686247dced9a6
michaelamoabengvaulitions <michael.amoabeng2015@gmail.com>
2024-06-24 18:56:03 +0300

    Removed unused imports

--- src/utils/getResponsiveSize.ts
+++ src/utils/getResponsiveSize.ts
@@ -20,12 +20,6 @@ export const wp: ResponsiveType = (value: number) => {
     return PixelRatio.roundToNearestPixel((SCREEN_WIDTH * dimension) / 100);
 };
 
-export const RESPONSIVE_SCREEN_HEIGHT = hp(SCREEN_HEIGHT);
-export const RESPONSIVE_SCREEN_WIDTH = wp(SCREEN_WIDTH);
-
-export const SPACE_XSMALL = wp(5);
 export const SPACE_SMALL = wp(10);
 export const SPACE_MEDIUM = wp(15);
 export const SPACE_LARGE = wp(20);
-export const SPACE_XLARGE = wp(25);
-export const SPACE_XXLARGE = wp(30);
